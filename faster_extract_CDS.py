72867907dbdf6ef4040c96845415ee31c4b9d660
Camilo García <cgarci39@eafit.edu.co>
2022-03-19 16:24:45 -0500

    add a faster script for extracting CDS from genbanks files using gb_io.py

---
+++ faster_extract_CDS.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import argparse
+import gb_io
+from rich import print
+
+def get_arguments():
+
+    parser = argparse.ArgumentParser(description='Count the frecuency of a k-mer set  given its size (k) along a genome or a set of genomes', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-i', '--input', type=str, help='Wirte the path to a genbank file')
+    parser.add_argument('-o', '--output', default="input-cds.fasta", help="Output a multifasta file", action="store")
+    parser.add_argument('-p', '--protein', type=str, help='Give a protein name or at least words that may match part of its complete name (it also finds the abreviation)')
+    
+    args = parser.parse_args()
+
+    return(args)
+
+input = args.input
+protein = args.protein
+output = open(args.output, "w")
+
+def main():
+    with open(output, "w") as out:
+        for record in gb_io.iter(input):
+            for feature in filter(lambda feat: feat.type == "CDS", record.features):
+                qualifiers = feature.qualifiers.to_dict()
+                if protein in qualifiers["locus_tag"][0]:
+                    start = feature.location.start.position + 1
+                    end = feature.location.end.position
+                    pos = [start, end] 
+                    out.write(">{}\n".format(qualifiers["locus_tag"][0]))
+                    out.write("{}\n".format(qualifiers["translation"][0]))
+
+
+if __name__ == '__main__':
+    main()
